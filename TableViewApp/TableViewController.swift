5628f1f133f16aab29f5548c224e703412955078
OoMorizono <x65f588d72sw76@gmail.com>
2021-10-31 17:55:41 +0900

    タスク追加です｡

---
+++ TableViewApp/TableViewController.swift
@@ -0,0 +1,110 @@
+//
+//  TableViewController.swift
+//  TableViewApp
+//
+//  Created by 森園王 on 2021/10/31.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    @IBOutlet var addText: UITextField!
+    var taskArray: [String] = []
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let userDefaults = UserDefaults.standard
+        if userDefaults.object(forKey: "add") != nil{
+            taskArray = userDefaults.object(forKey:"add") as! [String]}
+        print("viewDidLordから")
+        print(taskArray)
+    }
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        //"add"というキーで保存された値がなにかある -> 値をtaskArrayへ
+        let userDefaults = UserDefaults.standard
+        if userDefaults.object(forKey: "add") != nil {
+            taskArray = userDefaults.object(forKey:"add") as! [String]
+              }
+              //tableViewを更新
+              tableView.reloadData()
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return taskArray.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+             // セルのオブジェクトを作成します。
+              // "NameCell" の部分はStoryboardでセルに設定したIdentifierを指定しています。
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NameCell", for: indexPath)
+
+             // namesから該当する行の文字列を取得してセルに設定します。
+              // indexPath.rowで何行目かがわかります。
+        cell.textLabel?.text = taskArray[indexPath.row]
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
